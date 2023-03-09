4c1be846be69751c7f3d9735ddf3a08f6d053c91
unknown <juan.daza-pi@labinfo.is.escuelaing.edu.co>
2023-03-09 16:15:24 -0500

    Aplicacion funcionando

--- src/main/java/edu/eci/cvds/guess/GuessBean.java
+++ src/main/java/edu/eci/cvds/guess/GuessBean.java
@@ -2,35 +2,41 @@ package edu.eci.cvds.guess;
 
 import java.util.jar.Attributes.Name;
 
-import javax.annotation.ManagedBean;
-
 import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
 @ManagedBean(name = "guessBean")
-@ApplicationScoped
+@SessionScoped
 
 public class GuessBean {
     private int number;
     private int trys;
     private int prize;
     private String state;
+    private int guessNumber;
 
 
     public GuessBean() {
-
+        prize=100000;
+        trys=0;
+        state="Playing";
+        number=(int)((Math.random()*(10-1))+1);
     }
 
     public int getNumber(){
         return number;
     }
 
+    public int getGuessNumber(){
+        return guessNumber;
+    }
+
     public int getTrys(){
         return trys;
     }
 
-    public int getPrizes(){
+    public int getPrize(){
         return prize;
     }
 
@@ -41,13 +47,37 @@ public class GuessBean {
     public void setNumber(int number){
         this.number=number;
     }
+
+    public void setGuessNumber(int guessNumber){
+        this.guessNumber=guessNumber;
+    }
     public void setTrys(int trys){
         this.trys=trys;
     }
-    public void setprize(int prize){
+    public void setPrize(int prize){
         this.prize=prize;
     }
     public void setState(String state){
         this.state=state;
+    }
+
+    public void guess(){
+        if(guessNumber==number && !getState().equals("Loser")){
+            setState("Winner");
+
+        }
+        else if(!getState().equals("Loser")){
+            setTrys(getTrys()+1);
+            setPrize(getPrize()-10000);
+            if(getPrize()<1){
+                setState("Loser");
+            }
+        }
+    }
+    public void restart(){
+        setPrize(100000);
+        setTrys(0);
+        setState("Playing");
+        setNumber((int)((Math.random()*(10-1))+1));
     }
 }
